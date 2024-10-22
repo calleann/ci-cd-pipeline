f3dd8aea852db5c6aa72e549bae11a7f162a398f
calleann <anas.riyuu@gmail.com>
2024-10-22 22:44:39 +0200

    refractor layout

--- app.py
+++ app.py
@@ -6,21 +6,22 @@ from flask import Flask, render_template
 
 # Instantiate a new Flask app
 app = Flask(__name__)
+LAYOUT = "layout.html"
 
 
 @app.route("/")
 def get_df():
-    return render_template("layout.html", title="Ma dataset", html_content=df.to_html())
+    return render_template(LAYOUT, title="Ma dataset", html_content=df.to_html())
 
 
 @app.route("/scatter")
 def get_scatter_plot():
-    return render_template("layout.html", title="Le scatter plot", html_content=fig_scatter.to_html())
+    return render_template(LAYOUT, title="Le scatter plot", html_content=fig_scatter.to_html())
 
 
 @app.route("/hist")
 def get_hist_plot():
-    return render_template("layout.html", title="Histogramme", html_content=fig_hist.to_html())
+    return render_template(LAYOUT, title="Histogramme", html_content=fig_hist.to_html())
 
 
 if __name__ == "__main__":
